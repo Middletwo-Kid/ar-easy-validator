74734aa7135759281d561e457234c64515022b91
Middletwo-Kid <976989701@qq.com>
2020-12-18 15:24:46 +0800

    feat: 重构完成

--- dist/validator.js
+++ dist/validator.js
@@ -102,6 +102,8 @@
     isIdcard
   };
 
+  const sign = ['>', '>=', '==', '<', '<=', '!=', '!=='];
+
   function Validator(){
     const self = this;
 
@@ -110,12 +112,7 @@
       self[key] = hook[key];
     });
   }
-  Validator.prototype.validate = (formData, rule) => {
-    Object.keys(formData).forEach((key) => {
-      const val = formData[key];
-    });
-  };
-
+  // 新增校验方法
   Validator.prototype.addRules = (validateMethod, fn) => {
     // 校验名字是否合法
     if(typeof validateMethod !== 'string' || validateMethod in hook || validateMethod === 'addRules') {
@@ -130,6 +127,180 @@
     }
 
     Validator.prototype[validateMethod] = fn;
+  };
+
+  // 校验表单
+  Validator.prototype.validate = (formData, rules) => {
+    // 参数和校验规则都必须为对象
+    if(!(formData instanceof Object) || !(rules instanceof Object)) {
+      console.error(`method validate's args should be Object`);
+      return false;
+    } else if(!hook.isEmpty(rules) && hook.isEmpty(formData)) {
+      // 如果校验规则不为空，但是对象为空，直接返回false
+      return false
+    }
+    const ruleArr = formateRules(rules);
+
+    for(let i = 0 ; i < ruleArr.length; i++) {
+      const item = ruleArr[i];
+
+      if(!(item.field in formData)) {
+        // 如果这个key不存在formData, 跳过校验
+        continue;
+      } else {
+        let flag;
+        const value = formData[item.field];
+        const { field, rule, need: needRules, tip } = item;
+
+        if(needRules){
+          // 有前置条件，如果通过才进行下一步校验，否则跳过这次的校验    
+          
+          // need中的rules不符合规范，直接返回false
+          if(!(checkNeed(needRules))) return false;
+          
+          const needRuleArr = formateNeedRules(needRules);
+          let needFlag = true;
+
+          for(let j = 0 ; j < needRuleArr.length; j ++ ){
+            const needItem = needRuleArr[j];
+            const needValue = formData[needItem.field];
+            const needRule = needItem.rule;
+            needFlag = checkRule(needValue, needRule);
+            if(!needFlag) {
+              break;
+            }
+          }
+
+          if(needFlag) {
+            flag = checkRule(value, rule, tip);
+            throwResult(flag, field);
+          }else {
+            // 跳出此次循环
+            continue;
+          }
+        }else {
+          // 当没有前置条件的时候，直接判断是否校验成功
+          flag = checkRule(value, rule, tip);
+          throwResult(flag, field);
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const throwResult = (flag, field) => {
+    if(!flag) {
+      console.error(`${field} is error`);
+      return false;
+    }else {
+      return true;
+    }
+  };
+
+  // 格式化rules
+  const formateRules = (rules) => {
+    return Object.keys(rules).map(key => {
+      let obj = JSON.parse(JSON.stringify(rules[key]));
+      if(obj && hook.isEmpty(obj.rule)) obj.rule = 'isRequired';
+      else if(typeof obj === 'string') {
+        obj = {};
+        obj['rule'] = 'isRequired';
+      }
+      obj.field = key;
+      return obj;
+    });
+  };
+
+  const formateNeedRules = (rules) => {
+
+    return rules.map(item => {
+      const rule = item.rule;
+      if(hook.isEmpty(rule)) item.rule = 'isRequired';
+      else if(!(rule in hook)) item.rule = 'isRequired';
+      
+      return item;
+    })
+  };
+
+  const checkRule = (value, rule, canBeArray = true) => {
+    let type = '';
+
+    if(typeof rule === 'string'){
+      // { rule: 'isRequired' }
+      type = 'string';
+    }else if(canBeArray && Array.isArray(rule)){
+      // { rule: ['isRequired', 'isEmail' ]}
+      type = 'array';
+    }else if(Object.prototype.toString.call(rule) == '[object Object]') {
+      // { rule: {'==': 3}}
+      type = 'object';
+    }else {
+      console.error('rule is invalid');
+      return false;
+    }
+
+    let flag = false;
+    switch(type){
+      case 'object': flag = validateObjectRule(value, rule); break;
+      case 'array': flag = validateArrayRule(value, rule); break;
+      default: flag = validateStringRule(value, rule); break;
+    }
+
+    return flag;
+  };
+
+  const checkNeed = (rules) => {
+    if(!Array.isArray(rules) || ((Array.isArray(rules) && rules.length === 0))){
+      console.error('need is invalid');
+      return false;
+    }
+
+    return true;
+  };
+
+  const validateStringRule = (value, rule) => {
+    if(!(rule in hook)) {
+      console.error('rule is invalid');
+      return false;
+    }
+
+    let flag = true;
+    if(rule !== 'isRequired'){
+      flag = hook.isRequired(value);   
+    }
+
+    return flag && hook[rule](value);
+  };
+
+  const validateObjectRule = (value, rule) => {
+    const key = Object.keys(rule)[0];
+    if(!(sign.includes(key) > -1)) {
+      console.error('rule is invalid');
+      return false;
+    }
+    const val = rule[key];
+
+    switch (key) {
+      case '>': return (+value) > (+val);
+      case '>=': return (+value) >= (+val);
+      case '==': return value == val;
+      case '===': return value === val;
+      case '<': return (+value) < (+val);
+      case '<=': return (+value) <= (+val);
+      case '!=': return value != val;
+      case '!==': return value !== val;
+      default: return false;
+    }
+  };
+
+  const validateArrayRule = (value, rules) => {
+    for(let i = 0; i < rules.length; i++){
+      const rule = rules[i];
+      let flag = checkRule(value, rule, false);
+      if(!flag) return false;
+    }
+    return true;
   };
 
   return Validator;
--- rollup.config.js
+++ rollup.config.js
@@ -4,5 +4,11 @@ export default {
     format: 'umd',
     file: 'dist/validator.js',
     name: 'Validator'          // 导出的默认对象名字
+  },
+  onwarn: function (warning) {
+    if (warning.code === 'THIS_IS_UNDEFINED') {
+    return;
+  }
+    console.error(warning.message);
   }
 }
